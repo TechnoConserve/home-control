303ab648c92f453089b3538b3dc018b7735cfd06
Avery Uslaner <uslaner.avery@gmail.com>
2017-10-17 15:34:40 -0600

    Added scripts to control the red LED strip in the snake tank.

---
+++ red_on.py
@@ -0,0 +1,12 @@
+#!/home/pi/venv/home_control/bin/python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+pin = 17  # Red LED strip in snake tank
+
+GPIO.setup(pin, GPIO.OUT)
+GPIO.setup(pin, GPIO.LOW)
+print("And the world becomes just a little more red.")
+time.sleep(2)
