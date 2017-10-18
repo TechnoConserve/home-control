16f9fb4c6475dd1aa04e73faf83f25d2c092d34a
Avery Uslaner <uslaner.avery@gmail.com>
2017-10-17 22:43:23 -0600

    Removed test queue script and added queue checking script.

---
+++ check_queue.py
@@ -0,0 +1,42 @@
+import boto3
+import os
+from subprocess import call
+import time
+
+ACCESS_KEY = os.environ.get('ACCESS_KEY', '')
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET', '')
+REGION = 'us-east-1'
+QUEUE_URL = os.environ.get('QUEUE_URL', '')
+
+
+def pop_message(client, url):
+    response = client.receive_message(QueueUrl=url, MaxNumberOfMessages=10)
+
+    # last message posted becomes messages
+    message = response['Messages'][0]['Body']
+    receipt = response['Messages'][0]['ReceiptHandle']
+    client.delete_message(QueueUrl=url, ReceiptHandle=receipt)
+    return message
+
+
+client = boto3.client('sqs', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=ACCESS_SECRET, region_name=REGION)
+
+waittime = 20
+client.set_queue_attributes(QueueUrl=QUEUE_URL, Attributes={'ReceiveMessageWaitTimeSeconds': str(waittime)})
+
+time_start = time.time()
+while time.time() - time_start < 60:
+    print("Checking...")
+    try:
+        message = pop_message(client, QUEUE_URL)
+        print(message)
+        if message == "Main light on":
+            call(['/home/pi/controls/snake_on.py'])
+        elif message == "Main light off":
+            call(['/home/pi/controls/snake_off.py'])
+        elif message == "Secondary light on":
+            call(['/home/pi/controls/red_on.py'])
+        elif message == "Secondary light off":
+            call(['/home/pi/controls/red_off.py'])
+    except:
+        pass
