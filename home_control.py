8cf2c1d181d462fd6488d9f0bc9efb88b45df42c
Avery Uslaner <uslaner.avery@gmail.com>
2017-10-17 18:42:52 -0600

    Added checking to reduce the number of unnecessary calls to light scripts.

--- home_control.py
+++ home_control.py
@@ -18,6 +18,7 @@ def get_states():
     # Convert the strings into integers
     red_state = int(red_state)
     fluorescent_state = int(fluorescent_state)
+    # Flip the states because 1 equals off, which makes less intuitive sense
     states = {'red': not red_state, 'fluorescent': not fluorescent_state}
     return states
 
@@ -30,16 +31,20 @@ def home():
     form.snake_light.data = states['fluorescent']
     form.red_light.data = states['red']
     if request.method == 'POST' and form.validate():
+        # Flip the states here as well to be consistent with check_states function
         form_fluorescent = not form.snake_light.data
         form_red = not form.red_light.data
         if form_fluorescent != states['fluorescent']:
-            call(['/home/pi/controls/snake_on.py'])
-        else:
-            call(['/home/pi/controls/snake_off.py'])
+            # Only need to do something if the form option doesn't match the current state of the light
+            if form_fluorescent:
+                call(['/home/pi/controls/snake_on.py'])
+            else:
+                call(['/home/pi/controls/snake_off.py'])
         if form_red != states['red']:
-            call(['/home/pi/controls/red_on.py'])
-        else:
-            call(['/home/pi/controls/red_off.py'])
+            if form_red:
+                call(['/home/pi/controls/red_on.py'])
+            else:
+                call(['/home/pi/controls/red_off.py'])
     return render_template('control_home.html', form=form)
 
 
